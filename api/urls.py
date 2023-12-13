fbe9fe1fd5ffca6c881e206f093d6e03274a91b0
AM-5006 <aniketmandrekar72576@gmail.com>
2023-12-14 00:37:31 +0530

    Changes

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path, include
-from .views import save_user_info, get_user
+from .views import save_user_info
 
 urlpatterns = [
     path('save_info/', save_user_info),
-
-    #path('get_info/', get_user),
-    
 ]
