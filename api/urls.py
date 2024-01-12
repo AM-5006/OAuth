9dff173744979770520e3215b221b080d47b03e1
Aniket Mandrekar <113555994+AM-5006@users.noreply.github.com>
2024-01-12 09:46:29 +0530

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import save_user_info
+from .views import save_user_info, index
 
 urlpatterns = [
+    path('', index, 'index'),
     path('save_info/', save_user_info),
 ]
